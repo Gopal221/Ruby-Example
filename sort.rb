42f9e3bdb51eb002e3885ca7d850f9e8d82fac1a
Gopal221 <gopal.sharma@w3villa.com>
2016-03-07 16:36:26 +0530

    Update sort.rb

--- sort.rb
+++ sort.rb
@@ -1,3 +1,21 @@
 arr = [30, 40, 32, 55, 16, 48, 4]
 arr.sort! {|x, y| y <=> x}
 puts arr
+
+def my_sort(list)
+ (0..list.size).each do |j|
+    (0..list.size-2).each do |i|
+    	temp =0 
+      if list[i] > list[i+1]
+      	 temp = list[i]
+      	 list[i] = list[i+1]
+      	 list[i+1]= temp
+       	 swapped = true
+      end
+   end
+  end
+puts list
+end
+
+list = [30, 40, 32, 55, 16, 48, 4]
+my_sort(list)
