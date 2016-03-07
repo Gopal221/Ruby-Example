13b99bf9c4cabd09d49e1aa2cda20a536717fe87
Gopal221 <gopal.sharma@w3villa.com>
2016-03-07 15:17:08 +0530

    Update largest.rb

--- largest.rb
+++ largest.rb
@@ -5,3 +5,18 @@ end
 
 hash = { "n" => 100, "m" => 100, "y" => 300, "d" => 200, "a" => 0 }
 largest_hash_key(hash)
+
+#!/usr/bin/ruby
+
+def greater_hash_key(hash)
+	max =0 
+	hash.each do |key, value|
+		if value > max 
+			max = value
+		end
+  	end
+  	return max
+end
+
+
+p greater_hash_key(hash)
