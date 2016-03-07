0835ab7053c704cb898efc2ee7d39f3cb4a196ff
Gopal221 <gopal.sharma@w3villa.com>
2016-03-07 15:16:11 +0530

    Update sum.rb

--- sum.rb
+++ sum.rb
@@ -6,3 +6,17 @@ end
 
 hash = { "n" => 100, "m" => 100, "y" => 300, "d" => 200, "a" => 0 }
 sum_hash_key(hash)
+
+
+#!/usr/bin/ruby
+
+def sum_hash_key(hash)
+	sum =0 
+	hash.each do |key, value|
+		sum = sum + value
+  	end
+  	return sum
+end
+
+hash = { "n" => 100, "m" => 100, "y" => 300, "d" => 200, "a" => 0 }
+p sum_hash_key(hash)
